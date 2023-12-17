affea3274454992b657b6bbffa3543b0039d3f82
thanhnd <ducthanhnguyen95@gmail.com>
2023-12-17 11:02:17 +0700

    Demo reactive for spring boot

---
+++ src/test/java/com/example/testreactive/Player.java
@@ -0,0 +1,14 @@
+package com.example.testreactive;
+
+public class Player {
+
+    private final String firstName;
+    private final String lastName;
+
+    protected Player(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+
+}
